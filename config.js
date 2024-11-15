6f772946369f755cc21db606e882cddfec0da1d2
Shika <165547276+Ficcasso@users.noreply.github.com>
2024-11-16 00:47:10 +0100

    config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ DEBUG =
 module.exports = {
   VERSION: "v1.2.8",
   SESSION:
-    process.env.ASENA_SESSION === undefined ? "" : process.env.ASENA_SESSION,
+    process.env.ASENA_SESSION === undefined ? "levanter_1614af70e70be244cc95072fd2c34d74bc" : process.env.ASENA_SESSION,
   EXT: process.env.EXT === undefined ? undefined : process.env.EXT,
   LANG:
     process.env.LANGUAGE === undefined
